025c464a7d8af50bab09115f2a59f41ff2d45e53
Oliver Guo <oliguo@users.noreply.github.com>
2021-03-05 15:51:06 +0800

    Update APNS.php
    Change to APNS HTTP/2 API

--- core/APNS.php
+++ core/APNS.php
@@ -1,59 +1,63 @@
 <?php
 
-function iOSPush($message, $deviceToken, $datas=[], $pem,$passphrase) {
-    $ctx = stream_context_create();
-    stream_context_set_option($ctx, 'ssl', 'local_cert', $pem);
-    stream_context_set_option($ctx, 'ssl', 'passphrase', $passphrase);
+function iOSPush($message, $deviceToken, $datas = [], $pem, $passphrase)
+{
+    $API_URL = 'https://api.push.apple.com:443/3/device/';
+    //Production:https://api.push.apple.com:443/3/device/
+    //Development:https://api.development.push.apple.com:443/3/device/
+    $apns_topic = "com.membershiptrm.cyy.app"; //app bundle id
 
-    // Open a connection to the APNS server
-    $fp = stream_socket_client(
-            'ssl://gateway.push.apple.com:2195', $err,
-            //'ssl://gateway.sandbox.push.apple.com:2195',$err,
-            //gateway.sandbox.push.apple.com:2195 if use development cert
-            $errstr, 60, STREAM_CLIENT_CONNECT | STREAM_CLIENT_PERSISTENT, $ctx);
+    $curl = curl_init();
+    $payload = [
+        "aps" => [
+            'alert' => $message,
+            'badge' => 1,
+            'sound' => 'default',
+            'datas' => $datas
+        ]
+    ];
+    $post_fields = json_encode($payload);
+    curl_setopt_array($curl, array(
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_2_0,
+        CURLOPT_SSLCERT => $pem,
+        CURLOPT_SSLCERTPASSWD => $passphrase,
+        CURLOPT_SSLKEYTYPE => "PEM",
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_URL => $API_URL . $deviceToken,
+        CURLOPT_POSTFIELDS => $post_fields,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_HTTPHEADER => array(
+            "apns-topic:" . $apns_topic,
+            "apns-priority:10",
+            "apns-expiration:0"
+        ),
+    ));
 
-    //echo 'Connected to APNS' . PHP_EOL;
-    // Create the payload body
-    $body['aps'] = array(
-        'alert' => $message,
-        'badge' => 1,
-        'sound' => 'default',
-        'datas' => $datas
-    );
-    if (!$fp) {
+    curl_exec($curl);
+    $info = curl_getinfo($curl);
+    // print_r($info);
+    $body = [
+        'post' => $payload,
+        'info' => $info
+    ];
+    if ($info['http_code'] == 200) {
+        curl_close($curl);
         $details = [
-            "type" => "remark",
-            "remark" => "Failed to connect: $err $errstr",
+            "type" => "success_ios_token",
+            "remark" => "sent successfully:" . json_encode($body),
+            "value" => $deviceToken
+        ];
+        $feedbackArray = ['feedback' => 'success', 'details' => $details];
+        return json_encode($feedbackArray, JSON_UNESCAPED_SLASHES);
+    } else {
+        curl_close($curl);
+        $details = [
+            "type" => "fail_ios_token",
+            "remark" => json_encode($body),
             "value" => $deviceToken
         ];
         $feedbackArray = ['feedback' => 'error', 'details' => $details];
         return json_encode($feedbackArray, JSON_UNESCAPED_SLASHES);
-    } else {
-        // Encode the payload as JSON
-        $payload = json_encode($body);
-        // Build the binary notification
-        $msg = chr(0) . pack('n', 32) . pack('H*', $deviceToken) . pack('n', strlen($payload)) . $payload;
-        // Send it to the server
-        $result = fwrite($fp, $msg, strlen($msg));
-        if ($result) {
-            // Close the connection to the server
-            fclose($fp);
-            $details = [
-                "type" => "success_ios_token",
-                "remark" => "sent successfully:" . json_encode($body),
-                "value" => $deviceToken
-            ];
-            $feedbackArray = ['feedback' => 'success', 'details' => $details];
-            return json_encode($feedbackArray, JSON_UNESCAPED_SLASHES);
-        } else {
-            fclose($fp);
-            $details = [
-                "type" => "fail_ios_token",
-                "remark" => json_encode($body),
-                "value" => $deviceToken
-            ];
-            $feedbackArray = ['feedback' => 'error', 'details' => $details];
-            return json_encode($feedbackArray, JSON_UNESCAPED_SLASHES);
-        }
     }
 }
+
